00628ca54a5919d4a0448170c88618040c85fd3d
roger <roger.herikstad@gmail.com>
2016-09-27 15:54:51 +0800

    Fixes deprecation warnings

--- src/GUICheck.jl
+++ src/GUICheck.jl
@@ -1,12 +1,12 @@
 module GUICheck
 
 function hasgui()
-    username = split(readall(`who am i`))[1]
+    username = split(readstring(`who am i`))[1]
     return hasgui(username)
 end
 
 function hasgui(username::AbstractString)
-    s = readall(`who`)
+    s = readstring(`who`)
     return ismatch(Regex("$username[ \t]* console"),s)
 end
 
